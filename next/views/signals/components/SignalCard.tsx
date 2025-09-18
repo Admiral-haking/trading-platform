d9f4d2df5d134b77bb6d75c132529116ec27fa59
Youth Development Group <youthdev10313@gmail.com>
2025-09-18 21:41:25 +0330

    deploy

--- next/views/signals/components/SignalCard.tsx
+++ next/views/signals/components/SignalCard.tsx
@@ -144,7 +144,7 @@ export default function SignalCard({ s, markets }: { s: Signal, markets: Markets
                 />
                 <Detail
                   text='Updated At'
-                  value={getTime("updated_at" in s.coinex_position ? s.coinex_position.updated_at : s.createdAt)}
+                  value={getTime("updated_at" in s.coinex_position ? s.coinex_position.updated_at : s.updatedAt)}
                   icon={<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><g fill="currentColor" fillRule="evenodd" clipRule="evenodd"><path d="M17 17a5 5 0 1 0 0-10a5 5 0 0 0 0 10m.75-7a.75.75 0 0 0-1.5 0v1.846c0 .18.065.355.183.491l1 1.154a.75.75 0 0 0 1.134-.982l-.817-.943z"></path><path d="M1.25 7A.75.75 0 0 1 2 6.25h8a.75.75 0 0 1 0 1.5H2A.75.75 0 0 1 1.25 7m0 5a.75.75 0 0 1 .75-.75h6a.75.75 0 0 1 0 1.5H2a.75.75 0 0 1-.75-.75m0 5a.75.75 0 0 1 .75-.75h8a.75.75 0 0 1 0 1.5H2a.75.75 0 0 1-.75-.75" opacity={0.5}></path></g></svg>}
                 />
               </Box>
